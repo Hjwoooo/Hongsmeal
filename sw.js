ffa0758a157f425f14238551a4b35066eedebe7a
Hjwoooo <tingyu0901@goedu.kr>
2026-09-21 00:50:27 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // 웹앱 구동 지원
+});
